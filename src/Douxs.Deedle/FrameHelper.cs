787c98631f03f158d2ad67333864ded1da97adf5
兜小神 <kelely@vip.qq.com>
2021-08-25 22:31:53 +0800

    FromJsonArray

---
+++ src/Douxs.Deedle/FrameHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using EnsureThat;
+using Newtonsoft.Json.Linq;
+
+namespace Deedle
+{
+    public static class FrameHelper
+    {
+        public static Frame<int, string> FromJsonArray([NotNull] JArray objects)
+        {
+            Ensure.That(objects).IsNotNull();
+            Ensure.That(objects).HasItems();
+
+            // ReSharper disable once EnforceIfStatementBraces
+            if (objects.First is not JObject o)
+                throw new FormatException($"{nameof(objects)} 必须是一个包含 JObject 对象的数组!");
+
+            var rows = new List<KeyValuePair<int, Series<string, object>>>();
+            var properties = o.Properties().Select(p => p.Name).ToArray();
+
+            for (var i = 0; i < objects.Count; i++)
+            {
+                var sb = new SeriesBuilder<string> {{"Index", i}};
+
+                foreach (var property in properties)
+                {
+                    sb.Add(property, objects[i][property]!.Value<string>());
+                }
+
+                rows.Add(KeyValue.Create(i, sb.Series));
+            }
+
+            var df = Frame.FromRows(rows);
+            return df;
+        }
+    }
+}
---
+++ tests/Douxs.Deedle.Tests/FrameHelperTests.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json.Linq;
+using Shouldly;
+using Xunit;
+
+namespace Deedle
+{
+    public class FrameHelperTests
+    {
+        [Fact]
+        public void Test()
+        {
+            var array = JArray.Parse(@"[{'key1':123, 'key2':456, 'key3':654},{'key1':789, 'key2':562, 'key3':518}]");
+            var df = FrameHelper.FromJsonArray(array);
+            df.RowCount.ShouldBe(2);
+            df.ColumnCount.ShouldBe(4);
+        }
+    }
+}
